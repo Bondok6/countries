fb28e672fae978b40278748b1cf80919ddd149f6
Bondok6 <kirolousbondok6@gmail.com>
2022-01-29 04:42:22 +0200

    delete all comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './style.css';
+import "./style.css";
 
-const countriesContainer = document.querySelector('.countries');
-const countryInput = document.querySelector('#input');
+const countriesContainer = document.querySelector(".countries");
+const countryInput = document.querySelector("#input");
 
-const renderCountry = (data, className = '') => {
+const renderCountry = (data, className = "") => {
   const [lang] = Object.values(data.languages);
 
   const html = `
@@ -14,91 +14,60 @@ const renderCountry = (data, className = '') => {
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>🌆</span>${data.capital}</p>
       <p class="country__row"><span>👫</span>${(
-    +data.population / 1000000
-  ).toFixed(1)} people</p>
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
       <p class="country__row"><span>🗣️</span>${lang}</p>
 
     </div>
   </article>
   `;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.insertAdjacentHTML("beforeend", html);
   countriesContainer.style.opacity = 1;
 };
 
-const getJson = (url, errMsg) => fetch(url).then((res) => {
-  if (!res.ok) throw new Error(errMsg);
-  return res.json();
-});
+const getJson = (url, errMsg) =>
+  fetch(url).then((res) => {
+    if (!res.ok) throw new Error(errMsg);
+    return res.json();
+  });
 
 // Using Fetch
 const getCountryData = (country) => {
-  countriesContainer.innerHTML = '';
+  countriesContainer.innerHTML = "";
 
   // Get Country
-  getJson(`https://restcountries.com/v3.1/name/${country}`, 'Country Not Found')
+  getJson(`https://restcountries.com/v3.1/name/${country}`, "Country Not Found")
     .then((data) => {
       const [countryData] = data;
 
       renderCountry(countryData);
 
-      if (!countryData.borders) throw new Error('No Neighbour Found');
+      if (!countryData.borders) throw new Error("No Neighbour Found");
 
       const [neighbour] = countryData.borders;
 
       // Get neighbour
       return getJson(
         `https://restcountries.com/v3.1/alpha/${neighbour}`,
-        'Neighbour Not Found',
+        "Neighbour Not Found"
       );
     })
     .then((data) => {
       const [neighbourData] = data;
-      renderCountry(neighbourData, 'neighbour');
+      renderCountry(neighbourData, "neighbour");
     })
     .catch((err) => alert(err));
 };
 
-countryInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
+countryInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
     const countryInp = countryInput.value;
 
     if (!countryInp) return;
 
     getCountryData(countryInp);
 
-    countryInput.value = '';
+    countryInput.value = "";
   }
 });
-
-// Old Way using XMLHttpRequest()
-
-// const getCountry = function (country) {
-//   const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-//   request.send();
-
-//   request.addEventListener('load', function () {
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
-
-//     renderCountry(data);
-
-//     // Get neighbours
-//     const [neighbours] = data.borders;
-
-//     if (!neighbours) return;
-
-//     const request2 = new XMLHttpRequest();
-//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbours}`);
-//     request2.send();
-
-//     request2.addEventListener('load', function () {
-//       const [neighbourData] = JSON.parse(this.responseText);
-//       console.log(neighbourData);
-//       renderCountry(neighbourData, 'neighbour');
-//     });
-//   });
-// };
-
-// getCountry('egypt');
